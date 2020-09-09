821ca537491a2794dc9384a76a76259782e1cc2a
Roman Nasyrov <romnasi@gmail.com>
2020-09-10 02:59:04 +0300

    Добавляет карту через api

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
+//menu-burger
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -12,3 +13,23 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+
+//map
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init(){
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [59.938635, 30.323118],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 17,
+    controls: []
+  });
+}
