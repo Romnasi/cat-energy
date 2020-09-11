9f2e8586b5a8fd881d1eae788dc7e712f8e28119
Roman Nasyrov <romnasi@gmail.com>
2020-09-11 03:33:06 +0300

    Добавляет api карт, метку со своим изображением

--- source/js/script.js
+++ source/js/script.js
@@ -14,22 +14,38 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-
 //map
-// Функция ymaps.ready() будет вызвана, когда
-// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-ymaps.ready(init);
-function init(){
-  // Создание карты.
-  var myMap = new ymaps.Map("map", {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
-    center: [59.938635, 30.323118],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
+ymaps.ready(function () {
+
+  var myMap = new ymaps.Map('map', {
+    center: [59.938781, 30.321761],
     zoom: 17,
     controls: []
+  }, {
+    searchControlProvider: 'yandex#search'
+  }),
+
+  // Создаём макет содержимого.
+  MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+    '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+  ),
+
+  myPlacemark = new ymaps.Placemark([59.938606, 30.322981], {
+    hintContent: '«Кэт энерджи»',
+    balloonContent: '«Кэт энерджи» магазин функционального питания для котов<br>Санкт-Петербург ул. Большая Конюшенная, д. 19/8'
+  }, {
+    // Опции.
+    // Необходимо указать данный тип макета.
+    iconLayout: 'default#image',
+    // Своё изображение иконки метки.
+    iconImageHref: 'img/map-pin.png',
+    // Размеры метки.
+    iconImageSize: [124, 106],
+    // Смещение левого верхнего угла иконки относительно
+    // её "ножки" (точки привязки).
+    iconImageOffset: [-60, -100]
   });
-}
+
+  myMap.geoObjects
+  .add(myPlacemark);
+});
